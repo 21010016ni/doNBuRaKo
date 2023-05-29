d7a4f45f6b8e17567964904c50e77f48d52ee85f
21010016ni <105106328+21010016ni@users.noreply.github.com>
2023-05-29 12:28:29 +0900

    スプライトがStart時に自動でカメラの角度と合わせるように

--- Assets/Scripts/SpriteLock.cs
+++ Assets/Scripts/SpriteLock.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SpriteLock : MonoBehaviour
 {
+	private void Start()
+	{
+		transform.rotation = Camera.main.transform.rotation;
+	}
 	void Update()
 	{
 		transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles - transform.parent.transform.localRotation.eulerAngles);
